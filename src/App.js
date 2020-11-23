87ae1d7422809a48b142263c824694978c61f4e7
Itziar Zameza García <itziar.zameza@gmail.com>
2020-11-23 16:41:17 +0100

    run doncs

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import "./App.css";
+
+//import {Link,Route,Switch} from 'react-router-dom';
+
+//SI HAY API
+//import {fetchReasons}  from './services/ReasonsService';
+
+// class AppRoot extends React.Component {
+
+//   ...
+
+//   fetchNewReasons() {
+//     fetchReasons()
+//       .then(data => {
+//         this.setState({
+//           reasonsStore: data.reasons
+//         });
+//       });
+//   }
+
+function App() {
+  return <div>Hola</div>;
+
+  /*NAV
+li.. Link to="/">Home></link>
+li   Lint to "/Child/1">Child 1</Link>
+li   Lint to "/Child/2">Child 2</Link>
+*/
+
+  /*<Switch> 
+  <Route exact path="/" component={Home}/>
+  <Route exact path="/about" component={About}/>
+  </Switch> */
+}
+export default App;
