2810f1bef824c8877ce354c3f7b5919cb00670ce
Itziar Zameza García <itziar.zameza@gmail.com>
2020-11-23 23:37:48 +0100

    Run docs with some styles

--- docs/static/js/main.e006f380.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonptemplate=this.webpackJsonptemplate||[]).push([[0],{17:function(t,e,n){},18:function(t,e,n){},24:function(t,e,n){"use strict";n.r(e);var c=n(3),r=(n(0),n(9)),i=n.n(r);n(17),n(18);var o=function(){return Object(c.jsx)("div",{children:"Hola"})},s=n(11);i.a.render(Object(c.jsx)(s.a,{children:Object(c.jsx)(o,{})}),document.getElementById("root"))}},[[24,1,2]]]);
-//# sourceMappingURL=main.e006f380.chunk.js.map
