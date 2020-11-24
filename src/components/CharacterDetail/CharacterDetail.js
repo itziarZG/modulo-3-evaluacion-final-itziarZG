d27881ddafed550fe1be5be880e95d0303cd30c0
Itziar Zameza García <itziar.zameza@gmail.com>
2020-11-24 11:43:03 +0100

    add detail's styles & mediaqueries

--- src/components/CharacterDetail/CharacterDetail.js
+++ src/components/CharacterDetail/CharacterDetail.js
@@ -1,28 +1,47 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-// import ModalCard from "./ModalCard";
+import "./CharacterDetail.scss";
+import Logo from "../../images/Rick_and_Morty_-_logo_(English).png";
+
 const CharacterDetail = (props) => {
   const { episodes, origin, status, name, id, image, species } = props.info;
   return (
-    <article className="detail__card">
-      <Link to="./" style={{ textDecoration: "none", color: "inherit" }}>
-        {" "}
-        Volver{" "}
-      </Link>
-      <img
-        className="detail__card--img"
-        src={image}
-        alt={`Foto de ${name}`}
-        title={`Foto de ${name}`}
-      />
-      <div>
-        <h4 className="detail__card--title">{name}</h4>
-        <ul className="detail__card--info">
-          <li className="detail__card--li">{status}</li>
-          <li className="detail__card--li">{species}</li>
-          <li className="detail__card--li">{origin}</li>
-          <li className="detail__card--li">{episodes}</li>
-        </ul>
+    <article className="detail">
+      <img className="detail__title" src={Logo} alt="Logo Rick and Morty"></img>
+      <div className="detail__card">
+        <Link to="./" className="detail__card__button">
+          {"< "}
+          Volver{" "}
+        </Link>
+        <div className="detail__character">
+          <img
+            className="detail__character--img"
+            src={image}
+            alt={`Foto de ${name}`}
+            title={`Foto de ${name}`}
+          />
+          <div className="detail__character--texts">
+            <h3 className="detail__card--title">{name}</h3>
+            <ul>
+              <li>
+                <span className="bold">Status: </span>
+                {status}
+              </li>
+              <li>
+                <span className="bold">Species: </span>
+                {species}
+              </li>
+              <li>
+                <span className="bold">Origin: </span>
+                {origin}
+              </li>
+              <li>
+                <span className="bold">Episodes: </span>
+                {episodes}
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </article>
   );
