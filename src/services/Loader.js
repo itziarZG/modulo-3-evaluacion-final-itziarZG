dd7fef7ce6283fdbab87c367aa5066fabd948338
Itziar Zameza García <itziar.zameza@gmail.com>
2020-11-25 11:17:29 +0100

    add extra URL control with loader

--- src/services/Loader.js
+++ src/services/Loader.js
@@ -3,6 +3,11 @@ import React from "react";
 import "./Loader.scss";
 
 const Loader = () => {
-  return <div className="loading">Loading</div>;
+  return (
+    <div className="loading">
+      Loading
+      <div className="loader"></div>
+    </div>
+  );
 };
 export default Loader;
